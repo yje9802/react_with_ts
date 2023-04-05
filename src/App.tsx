231c38e2f3228186ca5a1866792ef9fd62ecc065
Ji-eun Yang <jidak1165@gmail.com>
2023-04-06 00:39:03 +0900

    Main home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
 
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,11 +1,84 @@
 import styled from "styled-components";
+import {Link} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+    background-color: white;
+    color: ${props => props.theme.bgColor};
+    margin-bottom: 10px;
+    border-radius: 15px;
+    a {
+        padding: 20px;
+        transition: color 0.2s ease-in;
+        display: block;
+    }
+    &:hover {
+        a {
+            color: ${props => props.theme.accentColor};
+        }
+    }
+`;
 
 const Title = styled.h1`
+    font-size: 48px;
     color: ${props => props.theme.accentColor};
 `
 
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
+
 function Coins() {
-    return <Title>Coins</Title>
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async() => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+    }, [])
+
+    return (
+        <Container>
+            <Header>
+                <Title>Coin</Title>
+            </Header>
+            {loading ? (<Loader>Loading...</Loader>) : (
+                <CoinList>
+                    {coins.map(coin => <Coin key={coin.id}><Link to={`/${coin.id}`}>{coin.name} &rarr;</Link></Coin>)}
+                </CoinList>
+            )}
+        </Container>
+    )
 }
 
 export default Coins;
